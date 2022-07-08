0851bfb5dd54aea0b7ab6fe2fe7c64cd9ed131e7
Samuel Belo <samuel.belo@al.infnet.edu.br>
2022-07-08 01:54:07 +0100

    Adds initial configuration (vue, vite, vuetify)

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+<style lang="scss">
+@import './styles/variables.scss';
+</style>
